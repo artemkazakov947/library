aae59078b310ff4f7eed66385069be2b3cd38d98
Artem Kazakov <artemkazakov947@gmail.com>
2022-09-21 16:32:54 +0300

    Updated README.md, settings, requirements.txt

--- library/settings.py
+++ library/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n90%5zp9boq%x%#y!qp9o2y9)c4@=-hz2l@k)xfu90h0ps48@8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-n90%5zp9boq%x%#y!qp9o2y9)c4@=-hz2l@k)xfu90h0ps48@8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1"
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'catalog.Author'
 
-LOGIN_REDIRECT_URL = '/catalog/'
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
